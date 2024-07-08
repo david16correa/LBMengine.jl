84a0769797531f359d240ea65610959be28328f0
david16correa <david16correa@gmail.com>
2024-07-07 21:11:52 -0600

    dev of animation functions

--- src/main.jl
+++ src/main.jl
@@ -11,15 +11,15 @@ main - non-trivial initial conditions
 
 len = 100;
 x = range(-1, stop = 1, length = len);
-ρ = [1. for i in x, j in x];
-#=u = [[1; 0.] for i in x, j in x];=#
-u = [[i; 0.] for i in x, j in x];
-#=u = [(i == j == 0) ? ([0,0]) : [-j; i]./sqrt(i^2 + j^2) for i in x, j in x];=#
+#=ρ = [1. for i in x, j in x];=#
+u = [[1; 0.] for i in x, j in x];
+#=u = [[i; 0.] for i in x, j in x];=#
+#=u = [(i == j == 0) ? ([0.,0]) : [-j; i]./sqrt(i^2 + j^2) for i in x, j in x];=#
 model = modelInit(ρ, u);
-
 @time for _ in 1:101
     LBMpropagate!(model);
 end
+LBManim8(model);
 
 #= ==========================================================================================
 =============================================================================================
@@ -56,8 +56,8 @@ t = 0
 
 #=t += 10=#
 t = 101
-ρ = massDensity(model; time = t);
-ρu = momentumDensity(model; time = t);
+ρ = massdensity(model; time = t);
+ρu = momentumdensity(model; time = t);
 #----------------------------------heatmap and colorbar---------------------------------
 z = norm.(ρu);
 fig, ax, hm = heatmap(x,x,z, alpha = 0.7,
@@ -66,11 +66,11 @@ fig, ax, hm = heatmap(x,x,z, alpha = 0.7,
     )
 );
 ax.xlabel = "x"; ax.ylabel = "y";
-Colorbar(fig[:, end+1], hm,
+colorbar(fig[:, end+1], hm,
     #=ticks = (-1:0.5:1, ["$i" for i ∈ -1:0.5:1]),=#
 );
 #--------------------------------------gradient---------------------------------------
-pos = [Point2(i,j) for i ∈ x[1:10:end] for j ∈ x[1:10:end]];
+pos = [point2(i,j) for i ∈ x[1:10:end] for j ∈ x[1:10:end]];
 vec = [ρu[i,j] for i ∈ eachindex(x)[1:10:end] for j ∈ eachindex(x)[1:10:end]];
 lengths = norm.(vec) .|> len -> (len == 0) ? (len = 1) : (len = len);
 vec = 0.05 .* vec ./ lengths;
