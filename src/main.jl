8d50139cebaeeb1eea0c7a3dbbd4c74e9c82180f
david16correa <david16correa@gmail.com>
2024-07-24 13:42:16 -0700

    minor changes

--- src/main.jl
+++ src/main.jl
@@ -28,16 +28,9 @@ ax.xlabel = "y"; ax.ylabel = "|u|";
 model.u[50, :] .|> norm |> v -> lines!(ax, model.spaceTime.x, v);
 fig
 
-
-#=    axis=(=#
-#=          title = "fluid speed, t = $model.time[end], x = $model.spaceTime.x[50]",=#
-#=    )=#
-#=)=#
-
-
 #= ==========================================================================================
 =============================================================================================
-main - non-trivial initial conditions (deprecated!)
+main - non-trivial initial conditions (DEPRECATED!)
 =============================================================================================
 ========================================================================================== =#
 
@@ -58,17 +51,17 @@ u = [gaussian(i,j) * [1.; 1]  for i in x, j in x];
 
 
 model = modelInit(ρ, u; 
-    walledDimensions = [-1]
+    walledDimensions = [-1],
     pressurizedDimensions = [ ],
 );
 
 model = modelInit(ρ, u; 
-    walledDimensions = [2]
+    walledDimensions = [2],
     pressurizedDimensions = [ ],
 );
 
 model = modelInit(ρ, u; 
-    walledDimensions = [ ]
+    walledDimensions = [ ],
     pressurizedDimensions = [ ],
 );
 
@@ -79,66 +72,3 @@ end
 @time anim8fluidVelocity(model);
 
 @time anim8massDensity(model);
-
-#= ==========================================================================================
-=============================================================================================
-main - trivial initial conditions
-=============================================================================================
-========================================================================================== =#
-
-len, dims = 100, 2; 
-ρ = [1. for _ in Array{Int64}(undef, (len for _ in 1:dims)...)];
-u = [[0. for _ in 1:dims] for _ in ρ];
-model = modelInit(ρ, u; τ = 1.);
-
-@time for _ in 1:101
-    LBMpropagate!(model);
-end
-
-#= ==========================================================================================
-=============================================================================================
-misc - graphical stuff
-=============================================================================================
-========================================================================================== =#
-
-t = 1
-
-t += 100 
-ρ = massDensity(model; time = t);
-fig, ax, hm = heatmap(ρ, 
-    axis=(
-      title = "mass density, t = $(model.time[t] |> x -> round(x; digits = 2))",
-    )
-);
-Colorbar(fig[1,2], hm);
-fig
-
-t = 0
-
-#=t += 10=#
-t = 101
-ρ = massdensity(model; time = t);
-ρu = momentumdensity(model; time = t);
-#----------------------------------heatmap and colorbar---------------------------------
-z = norm.(ρu);
-fig, ax, hm = heatmap(x,x,z, alpha = 0.7,
-    axis=(
-        title = "momentum density, t = $(model.time[t])",
-    )
-);
-ax.xlabel = "x"; ax.ylabel = "y";
-colorbar(fig[:, end+1], hm,
-    #=ticks = (-1:0.5:1, ["$i" for i ∈ -1:0.5:1]),=#
-);
-#--------------------------------------gradient---------------------------------------
-pos = [point2(i,j) for i ∈ x[1:10:end] for j ∈ x[1:10:end]];
-vec = [ρu[i,j] for i ∈ eachindex(x)[1:10:end] for j ∈ eachindex(x)[1:10:end]];
-lengths = norm.(vec) .|> len -> (len == 0) ? (len = 1) : (len = len);
-vec = 0.05 .* vec ./ lengths;
-arrows!(fig[1,1], pos, vec, 
-    arrowsize = 10, 
-    align = :center
-);
-t
-fig
-
