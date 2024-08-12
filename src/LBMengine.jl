ad73e688b55eec1d4621d1b2de8e09946b4f4aed
David Correa <david.correa.bsc@gmail.com>
2024-08-11 22:52:37 -0600

    unnecessary methods are no longer exported

--- src/LBMengine.jl
+++ src/LBMengine.jl
@@ -7,32 +7,15 @@ module LBMengine
     include("aux.jl");
     include("functions.jl");
 
-    #= (!isdir("anims")) ? (mkdir("anims")) : nothing; (!isdir("anims/$(today())")) ? (mkdir("anims/$(today())")) : nothing; =#
-    #= (!isdir("figs")) ? (mkdir("figs")) : nothing; (!isdir("figs/$(today())")) ? (mkdir("figs/$(today())")) : nothing; =#
-
     # structs
     export LBMvelocity, LBMdistributions, LBMmodel
 
-    # aux methods - unsure
-    export dot, scalarFieldTimesVector, vectorFieldDotVector, vectorFieldDotVectorField
-    export pbcIndexShift, pbcMatrixShift, wallNodes, bounceBackPrep, save_jpg
-    export D1Q3, D2Q9, D3Q27
-
-    # main methods - unsure
-    export massDensityGet, momentumDensityGet, hydroVariablesUpdate
-    export equilibriumDistribution, collisionOperator, guoForcingTerm, tick!
-    export findInitialConditions
-
-    # aux methods - misc
-    export mean, norm
     # aux methods - graphics stuff
     export plotFluidVelocity, plotMomentumDensity, plotMassDensity
     export anim8fluidVelocity, anim8momentumDensity, anim8massDensity
 
-    # main methods - dynamics
-    export LBMpropagate!
     # main methods - initialization
     export modelInit
-
-    greet() = print("Hello World 42!")
+    # main methods - dynamics
+    export hydroVariablesUpdate, tick!, LBMpropagate!
 end # module LBMengine
